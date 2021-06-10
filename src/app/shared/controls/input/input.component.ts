e1f1d6a68204321b95a1aca7429753abcba58545
AyeshaQayyum123 <78010568+AyeshaQayyum123@users.noreply.github.com>
2021-06-10 15:31:53 +0500

    bootstrap added

--- src/app/shared/controls/input/input.component.ts
+++ src/app/shared/controls/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'Input1',
@@ -7,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
 
 })
 export class InputComponent implements OnInit{
-
+@Input() type: string = "text";
     constructor(){
 
     }
