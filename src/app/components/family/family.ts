50d8d5b4d0ded7fbb3176203822bf7aa4e278433
iramshahzadi <85546738+iramshahzadi@users.noreply.github.com>
2021-06-15 18:32:32 +0500

    employee personal information
    add 4 components  in personal info

---
+++ src/app/components/family/family.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'family-info',
+  templateUrl: './family.component.html',
+  styleUrls: ['./family-info.component.scss']
+})
+export class FamilyInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/pets/pet.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pets',
+  templateUrl: './pet.component.html',
+  styleUrls: ['./pet-info.component.scss']
+})
+export class PetInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
